b759b313838434a6ccb90c2a804c457ce6779b76
Albert Lindsay <>
2012-05-01 16:19:59 +0900

    Amoeba
    0.1.1

--- Amoeba/Properties/AssemblyInfo.cs
+++ Amoeba/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.0.3891")] // 2NiocD9HPfmIi9KcsEMkhQ029y/UHj6aA7QLawDMSgFG6F7IXNPN6ZzXBPHWHLqWFaTcHF28HtBc9B7jGcqUDQ==
+[assembly: AssemblyVersion("0.0.0.3892")] // 1DFENM8kzJqHiEydxMeffBrl44jD1y3o7u3C4v4+eXXAoSPYsx9SEWRDef6Y6UJU4FHYJCq6Foat2yiMM4i+hg==
