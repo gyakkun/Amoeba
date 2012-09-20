241166357233ac42aaa30b79f460a90c78fbbd3d
Lyrise <>
2012-09-21 04:06:36 +0900

    Amoeba
    バージョンアップ

--- Amoeba/Properties/AssemblyInfo.cs
+++ Amoeba/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.0.5096")] // KbOZ+nj3QDxcy43YXSrCWBm8O+HrX9IfRTBHlk1x/+lNL7KrK3DsnYH9UtnJ8CcUF2EKV5VS8jzqw+W9kPxQaQ==
+[assembly: AssemblyVersion("0.0.0.5097")] // 0Le2uQQCBLUeJrbNc1RupNiy0WgzkcpS25HXm13jk3ndgD7xAJz8zFvF2b4rhbfXZFqS2X1hFuQpc19Fz6KHqA==
