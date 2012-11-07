55b013dcb8959199389675de14146291337f6f15
Lyrise <>
2012-11-07 23:49:59 +0900

    Amoeba
    バージョンアップ

--- Amoeba/App.xaml.cs
+++ Amoeba/App.xaml.cs
@@ -36,7 +36,7 @@ namespace Amoeba
         {
             //System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
 
-            App.AmoebaVersion = new Version(0, 1, 42);
+            App.AmoebaVersion = new Version(0, 1, 43);
 
             Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
 
