02db523c86f3ca2e372f11aab8b1f4f38c7308d6
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2013-04-21 18:14:41 +0900

    update

--- Amoeba/Properties/AssemblyInfo.cs
+++ Amoeba/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.0.5531")] // uzigX0ZtiodJaCQNKQslRgj4vtI3Ioy0M4lmd73owh8gvwGPbxdmXNbncZtqVI9Tmu3YXElyzfQh/rQC41yXaw==
+[assembly: AssemblyVersion("0.0.0.5532")] // HIeKO14ND9Pex4wXGURt3whkVkddXNNCUTN3WxqtePg9wJ8Ctei9PjicXCZ0nskaaYOW031a9WPJFCF9D3nGZA==
