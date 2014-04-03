ba300904b8df3797d58fb4f28cf213022d6c308a
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2014-04-03 18:03:30 +0900

    Fix
    微修正。

--- Run_ConnectTest.bat
+++ Run_ConnectTest.bat
@@ -7,4 +7,4 @@ md "Amoeba\bin\Test"
 rd /s /q "Amoeba\bin\Test\Core"
 xcopy %target% "Amoeba\bin\Test\Core" /c /s /e /q /h /i /k /r /y
 
-call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Test\Core\Amoeba.exe"  "Amoeba\bin\Test\Core"
+call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Test\Core\Amoeba.exe" "Amoeba\bin\Test\Core"
--- Run_NetworkTest.bat
+++ Run_NetworkTest.bat
@@ -19,7 +19,7 @@ md "Amoeba\bin\Debug_4"
 rd /s /q "Amoeba\bin\Debug_4\Core"
 xcopy %target% "Amoeba\bin\Debug_4\Core" /c /s /e /q /h /i /k /r /y
 
-call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_1\Core\Amoeba.exe"  "Amoeba\bin\Debug_1\Core"
-call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_2\Core\Amoeba.exe"  "Amoeba\bin\Debug_2\Core"
-call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_3\Core\Amoeba.exe"  "Amoeba\bin\Debug_3\Core"
-call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_4\Core\Amoeba.exe"  "Amoeba\bin\Debug_4\Core"
+call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_1\Core\Amoeba.exe" "Amoeba\bin\Debug_1\Core"
+call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_2\Core\Amoeba.exe" "Amoeba\bin\Debug_2\Core"
+call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_3\Core\Amoeba.exe" "Amoeba\bin\Debug_3\Core"
+call "..\Library\Library.Tool\bin\Debug\Library.Tool.exe" "run" "Amoeba\bin\Debug_4\Core\Amoeba.exe" "Amoeba\bin\Debug_4\Core"
