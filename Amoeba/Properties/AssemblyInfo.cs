31d138109b0d4ce54a69052b17c32e679cb145e0
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2015-08-18 18:55:57 +0900

    Fix
    CapBaseをCapへ名称変更

--- Amoeba/Properties/AssemblyInfo.cs
+++ Amoeba/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.0.6940")] // rNoRELWMgN9RTa4MFHsWcTF645/zgPyYc4FPJYFarTU=
+[assembly: AssemblyVersion("0.0.0.6941")] // /MrjpJgltiThu5wcprqoPsuUZk78NYGfO4OTz8jB7ME=
