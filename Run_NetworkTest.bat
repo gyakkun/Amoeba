d972baf0719f5f34e0129636dbc5d92930695fa0
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2015-09-15 01:33:25 +0900

    Fix

--- Run_NetworkTest.bat
+++ Run_NetworkTest.bat
@@ -1,25 +1,26 @@
 set BATDIR=%~dp0
 cd %BATDIR%
 
-set target="Amoeba\bin\Debug\Core"
+set TOOL="C:\Local\Projects\Alliance-Network\Library\Library.Tools\bin\Debug\Library.Tools.exe"
+set TARGET="Amoeba\bin\Debug\Core"
 
 md "Amoeba\bin\Debug_1"
 rd /s /q "Amoeba\bin\Debug_1\Core"
-xcopy %target% "Amoeba\bin\Debug_1\Core" /c /s /e /q /h /i /k /r /y
+xcopy %TARGET% "Amoeba\bin\Debug_1\Core" /c /s /e /q /h /i /k /r /y
 
 md "Amoeba\bin\Debug_2"
 rd /s /q "Amoeba\bin\Debug_2\Core"
-xcopy %target% "Amoeba\bin\Debug_2\Core" /c /s /e /q /h /i /k /r /y
+xcopy %TARGET% "Amoeba\bin\Debug_2\Core" /c /s /e /q /h /i /k /r /y
 
 md "Amoeba\bin\Debug_3"
 rd /s /q "Amoeba\bin\Debug_3\Core"
-xcopy %target% "Amoeba\bin\Debug_3\Core" /c /s /e /q /h /i /k /r /y
+xcopy %TARGET% "Amoeba\bin\Debug_3\Core" /c /s /e /q /h /i /k /r /y
 
 md "Amoeba\bin\Debug_4"
 rd /s /q "Amoeba\bin\Debug_4\Core"
-xcopy %target% "Amoeba\bin\Debug_4\Core" /c /s /e /q /h /i /k /r /y
+xcopy %TARGET% "Amoeba\bin\Debug_4\Core" /c /s /e /q /h /i /k /r /y
 
-call "..\Library\Library.Tools\bin\Debug\Library.Tools.exe" "run" "Amoeba\bin\Debug_1\Core\Amoeba.exe" "Amoeba\bin\Debug_1\Core"
-call "..\Library\Library.Tools\bin\Debug\Library.Tools.exe" "run" "Amoeba\bin\Debug_2\Core\Amoeba.exe" "Amoeba\bin\Debug_2\Core"
-call "..\Library\Library.Tools\bin\Debug\Library.Tools.exe" "run" "Amoeba\bin\Debug_3\Core\Amoeba.exe" "Amoeba\bin\Debug_3\Core"
-call "..\Library\Library.Tools\bin\Debug\Library.Tools.exe" "run" "Amoeba\bin\Debug_4\Core\Amoeba.exe" "Amoeba\bin\Debug_4\Core"
+IF EXIST %TOOL% call %TOOL% "run" "Amoeba\bin\Debug_1\Core\Amoeba.exe" "Amoeba\bin\Debug_1\Core"
+IF EXIST %TOOL% call %TOOL% "run" "Amoeba\bin\Debug_2\Core\Amoeba.exe" "Amoeba\bin\Debug_2\Core"
+IF EXIST %TOOL% call %TOOL% "run" "Amoeba\bin\Debug_3\Core\Amoeba.exe" "Amoeba\bin\Debug_3\Core"
+IF EXIST %TOOL% call %TOOL% "run" "Amoeba\bin\Debug_4\Core\Amoeba.exe" "Amoeba\bin\Debug_4\Core"
