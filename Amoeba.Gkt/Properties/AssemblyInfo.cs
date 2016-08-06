d0f77114815a2def71f431e1435daf47e47af8f5
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2016-08-06 11:42:59 +0900

    Update
    Gtkプロジェクト追加。

---
+++ Amoeba.Gkt/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Amoeba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amoeba")]
+[assembly: AssemblyCopyright("Copyright © Lyrise 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.0.0.0")]
