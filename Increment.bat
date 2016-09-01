4c75a307494c714bf5b36162b60a978aacfa83f0
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2016-09-01 11:39:33 +0900

    batファイルを相対パスへ変更。

--- Increment.bat
+++ Increment.bat
@@ -1,10 +1,13 @@
-set LIBRARY_INCREMENT="C:\Local\Projects\Alliance-Network\Library\Increment.bat"
+set BATDIR=%~dp0
+cd %BATDIR%
+
+set LIBRARY_INCREMENT="..\Library\Increment.bat"
 IF EXIST %LIBRARY_INCREMENT% call %LIBRARY_INCREMENT%
 
 set BATDIR=%~dp0
 cd %BATDIR%
 
-set TOOL="C:\Local\Projects\Alliance-Network\Library\Library.Tools\bin\Debug\Library.Tools.exe"
+set TOOL="..\Library\Library.Tools\bin\Debug\Library.Tools.exe"
 IF NOT EXIST %TOOL% exit
 
 call %TOOL% "Increment" Amoeba.Wpf\Amoeba.Wpf.csproj Properties\AssemblyInfo.cs
