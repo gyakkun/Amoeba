a72374a9eb9eee8f63846d963089d41879f9532b
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2016-09-20 12:59:54 +0900

    Fix
    Electronのコードを追加。

---
+++ Electron/electron-build.js
@@ -0,0 +1,33 @@
+var packager = require('electron-packager');
+var config = require('./package.json');
+
+var options = {
+    dir: '.',
+    out: './dist',
+    name: 'amoeba',
+    platform: 'linux',
+    arch: 'x64',
+    version: '1.3.3',
+    icon: './src/images/amoeba.png',
+    'app-version': config.version,
+    'version-string': {
+        CompanyName: '',
+        FileDescription: '',
+        OriginalFilename: config.name,
+        FileVersion: config.version,
+        ProductVersion: config.version,
+        ProductName: config.name,
+        InternalName: config.name
+    },
+    overwrite: true,
+    asar: true,
+    prune: true,
+    ignore: "node_modules/(electron-packager|electron-prebuilt)|dist",
+};
+
+packager(options, function (err, appPath) {
+    if (err) {
+        throw err;
+    }
+    console.log('Done');
+});
---
+++ Electron/src/scripts/react/app.tsx
@@ -0,0 +1,34 @@
+/// <reference path="../typings.ts" />
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ButtonToolbar,　Button } from 'react-bootstrap';
+
+//import Hello from "./scripts/Hello.tsx";
+
+const buttonsInstance = (
+    <ButtonToolbar>
+        {/* Standard button */}
+        <Button>Default</Button>
+
+        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+        <Button bsStyle="primary">Primary</Button>
+
+        {/* Indicates a successful or positive action */}
+        <Button bsStyle="success">Success</Button>
+
+        {/* Contextual button for informational alert messages */}
+        <Button bsStyle="info">Info</Button>
+
+        {/* Indicates caution should be taken with this action */}
+        <Button bsStyle="warning">Warning</Button>
+
+        {/* Indicates a dangerous or potentially negative action */}
+        <Button bsStyle="danger">Danger</Button>
+
+        {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+        <Button bsStyle="link">Link</Button>
+    </ButtonToolbar>
+);
+
+ReactDOM.render(buttonsInstance, document.getElementById('app'));
