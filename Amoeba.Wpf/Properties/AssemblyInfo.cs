e9666c67f6e7688b810a4460bed44feb3778cd74
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2016-11-14 05:34:10 +0900

    Update
    バージョンアップ。

--- Amoeba.Wpf/Properties/AssemblyInfo.cs
+++ Amoeba.Wpf/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.0.7566")] // 4LXfB8Mgw9xkY1y5_oywly0er3UQpg8zA7KvU0tWBM8
+[assembly: AssemblyVersion("0.0.0.7567")] // E2yYkfswnJN_CeYS15Q_T1vl4aebZj72It83OycPDlc
--- Amoeba.Wpf/ServiceManager.cs
+++ Amoeba.Wpf/ServiceManager.cs
@@ -30,7 +30,7 @@ namespace Amoeba
 
         public ServiceManager()
         {
-            this.AmoebaVersion = new Version(4, 0, 36);
+            this.AmoebaVersion = new Version(4, 0, 37);
 
             this.Paths = new Dictionary<string, string>();
 
