0ac5a8b21d90e6e0aed55d2578e640a1cbd0c506
Lyrise <al.pyfgcrl.qjkxbmwvz@gmail.com>
2017-02-04 17:52:19 +0900

    Fix
    とりあえず。

---
+++ Amoeba.Interface/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Amoeba.Interface")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amoeba.Interface")]
+[assembly: AssemblyCopyright("Copyright © Lyrise 2009-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("0.0.0.0")]
+
+[assembly: InternalsVisibleTo("Amoeba.UnitTest")]
---
+++ Amoeba.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Amoeba.Service")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amoeba.Service")]
+[assembly: AssemblyCopyright("Copyright © Lyrise 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8796ee1c-25d5-483e-b273-a17710a61259")]
+[assembly: AssemblyVersion("0.0.0.0")]
+
+[assembly: InternalsVisibleTo("Amoeba.UnitTest")]
