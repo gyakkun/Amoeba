a8b346c1ad0de2d0585001b6731192ac2acf4881
Lyrise <lyrise1984@gmail.com>
2017-04-02 10:10:44 +0900

    Fix
    OpitonsWindowsを若干編集。

--- Amoeba.Interface/Sources/_Resources/Languages/_Generator/LanguagesManager.generated.cs
+++ Amoeba.Interface/Sources/_Resources/Languages/_Generator/LanguagesManager.generated.cs
@@ -14,6 +14,9 @@ namespace Amoeba.Interface
         public string Languages_Japanese { get { return this.Translate("Languages_Japanese"); } }
         public string MainWindow_Info { get { return this.Translate("MainWindow_Info"); } }
         public string MainWindow_Store { get { return this.Translate("MainWindow_Store"); } }
+        public string OptionsWindow_Connection { get { return this.Translate("OptionsWindow_Connection"); } }
+        public string OptionsWindow_TcpConnection { get { return this.Translate("OptionsWindow_TcpConnection"); } }
+        public string OptionsWindow_I2pConnection { get { return this.Translate("OptionsWindow_I2pConnection"); } }
         public string InfoControl_Type { get { return this.Translate("InfoControl_Type"); } }
     }
 }
---
+++ Languages.bat
@@ -0,0 +1,7 @@
+set BATDIR=%~dp0
+cd %BATDIR%
+
+set TOOL="Omnius\Omnius.Tools\bin\Debug\Omnius.Tools.exe"
+IF NOT EXIST %TOOL% exit
+
+call %TOOL% "Languages" "Amoeba.Interface\Resources\Languages"
