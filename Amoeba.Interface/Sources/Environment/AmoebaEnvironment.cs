4d832221fb4ff12010608337479f2fa579fb6c1f
Lyrise <lyrise1984@gmail.com>
2017-07-31 15:08:56 +0900

    Update
    App.configから不要な設定を削除。

--- Amoeba.Interface/Sources/Environment/AmoebaEnvironment.cs
+++ Amoeba.Interface/Sources/Environment/AmoebaEnvironment.cs
@@ -22,7 +22,7 @@ namespace Amoeba.Interface
             try
             {
                 Variables = new EnvironmentVariables();
-                Version = new Version(5, 0, 23);
+                Version = new Version(5, 0, 24);
                 Paths = new EnvironmentPaths();
                 Icons = new EnvironmentIcons();
                 Images = new EnvironmentImages();
